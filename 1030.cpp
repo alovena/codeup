a2054f29aa8b9fa2dc501c01ada3da723d949247
cehs0703 <cehs0703@naver.com>
2018-06-26 01:56:48 +0900

    commit

---
+++ 1030.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(void){
+	long long int n;
+	cin>>n;
+	cout<<n;
+}
---
+++ 1031.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(void){
+	int input;
+	std::cin>>input;
+	std::cout<<std::oct<<input;
+}
